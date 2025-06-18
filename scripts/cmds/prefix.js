2fb0150b38aed92b63d589526c37540f5a6f6e7c
gamingboymeheraz <gamingboymeheraz@gmail.com>
2025-06-19 00:46:10 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -116,9 +116,9 @@ module.exports = {
           "╔══『 𝐏𝐑𝐄𝐅𝐈𝐗 』══╗\n"
         + `║ 🌍 System : ${globalPrefix}\n`
         + `║ 💬 Chatbox : ${threadPrefix}\n`
-        + `║ ➤ ${threadPrefix}help to see all available cmds 🥵\n`
+        + `║ ➤ ${threadPrefix}help to see all available cmds \n`
         + "╚═══════════════╝",
-        attachment: await utils.getStreamFromURL("https://files.catbox.moe/8ufjqu.jpg")
+        attachment: await utils.getStreamFromURL("https://files.catbox.moe/t23564.jpg")
       });
     }
   }
